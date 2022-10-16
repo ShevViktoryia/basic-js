b4f3c07ada78fc08a967db7a85d9d4302d027b8d
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2022-10-16 17:12:01 +0300

    feat: implement getEmailDomain() function

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
+function deleteDigit(/*n*/) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  // let arr = [];
+  // n = String(n).split('');
+  // let min_elem = 0;
+  // min_elem = Math.min(...n);
+  // for(let i = 0; i < n.length; i++) {
+  //   if(n.join('').match(new RegExp(min_elem, 'g').length === 2)) {
+  //     arr.push(n[i]);
+  //     i+=1;
+  //   }
+  //   else if(n[i] != min_elem) {
+  //     arr.push(n[i]);
+  //   }
+  // }
+  // // arr1.push(n.slice(0, n.indexOf(min_elem)));
+  // // arr2.concat(n.slice(n.indexOf(min_elem+1), n.length));
+  // // return arr;
+  // arr = Number(arr.join(''));
+  // n = n.join('');
+  // if(arr > Number(n.slice(0, n.length-2)) && arr > Number(n.slice(1, n.length-1))) {
+  //   return arr;
+  // }
+  // else if(arr < Number(n.slice(0, n.length-2))) {
+  //   return Number(n.slice(0, n.length-1));
+  // }
+  // else {
+  //   return Number(n.slice(1, n.length-1));
+  // }
 }
 
 module.exports = {
